b33998f49fedf2333790fe363c35215798ca4129
zyt <2098557118@qq.com>
2025-01-16 22:58:06 +0800

    快速排序

---
+++ primary/quick_sort.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+void quick_sort(int q[],int l,int r){
+  if(l>=r)return;
+  int i = l-1;
+  int r = r+1;
+  int x = q[l+r>>1];
+  do i++; while(q[i]<x);
+  do j++; while(q[j]>x);
+  if(i<j){
+    swap(q[i],q[j]);
+  }
+  quick_sort(q,l,j);
+  quick_sort(q,j+1,r);
+
+}
